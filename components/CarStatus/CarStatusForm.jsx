42536d9a93635eb9af82601b56537b5b634cc38c
MD SHAHBAZ ANSARI <MDSHAHBAZ8298@GMAIL.COM>
2025-06-17 01:55:55 +0530

    phone input updated

--- components/CarStatus/CarStatusForm.jsx
+++ components/CarStatus/CarStatusForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
-// import 'react-phone-input-2/lib/material.css'
+import 'react-phone-input-2/lib/material.css'
 
 import {
   FaUser,
