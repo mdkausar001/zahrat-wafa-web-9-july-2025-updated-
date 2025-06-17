a87fc8ace5184d285c15f32dfddb4d5f449f1804
MD SHAHBAZ ANSARI <MDSHAHBAZ8298@GMAIL.COM>
2025-06-17 17:24:05 +0530

    phone input updated but t not work on vdrcel again and again

--- components/PhoneInputWrapper.js
+++
@@ -1,8 +0,0 @@
-// components/PhoneInputWrapper.js
-import dynamic from 'next/dynamic'
-import 'react-phone-input-2/lib/style.css'
-
-// Dynamically import the PhoneInput only on the client
-const PhoneInput = dynamic(() => import('react-phone-input-2'), { ssr: false })
-
-export default PhoneInput
